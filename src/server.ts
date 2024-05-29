593ae8fed5e5636e457a2cdbc476c3521dfd8d31
bishalthapa2055 <bishalthapa2055@gmail.com>
2024-05-29 11:23:24 +0545

    cahge

--- src/server.ts
+++ src/server.ts
@@ -8,16 +8,16 @@ import config from './config';
 const start = async() =>{
     console.log("Starting up........");
     const server = require("http").createServer(app);
-    try {
-        const mongodb = config.app.mongoUrl;
-        mongoose.set("strictQuery", true);
-        await mongoose.connect(mongodb).then((res) => {
-          console.log("Connected to mongoDb sucessfully");
-        });
+    // try {
+    //     const mongodb = config.app.mongoUrl;
+    //     mongoose.set("strictQuery", true);
+    //     await mongoose.connect(mongodb).then((res) => {
+    //       console.log("Connected to mongoDb sucessfully");
+    //     });
     
-      } catch (e) {
-        console.log("ERROR :", e);
-    }
+    //   } catch (e) {
+    //     console.log("ERROR :", e);
+    // }
       server.listen(process.env.port, () => {
         console.log(
           `server sttarted at port: http://${process.env.HOST_URL}:${process.env.PORT}`
